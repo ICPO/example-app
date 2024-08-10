34f089f2cc9987a59cf20f7127d70b7007f76232
ICPO <infuzia216@gmail.com>
2024-08-10 19:39:00 +1000

    Миграции по уроку и заданию

---
+++ database/migrations/2024_08_10_083002_create_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->index()->constrained('users');
+            $table->string('title',255);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
